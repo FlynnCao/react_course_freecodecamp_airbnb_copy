228147ba6677aa2892346cbc195699505569ddfa
Flynn <ppstream123@126.com>
2022-06-25 21:26:27 +0800

    feat: learn

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import logo from './logo.svg';
-import NavBar from './components/NavBar';
-import Hero from './components/Hero';
-import Card from './components/Card';
-import './App.css';
-
+// import logo from "./logo.svg";
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
+import "./App.css";
 function App() {
-	return (
-		<div className="App">
-			<NavBar></NavBar>
-			<Hero></Hero>
-			<Card></Card>
-		</div>
-	);
+  const colors = ["Red", "Orange", "Purple", "Yellow", "Green"];
+  const targetColor = colors.map((color) => <h3>{color}</h3>);
+  return (
+    <div className="App">
+      <div>{targetColor}</div>
+      <NavBar> </NavBar> <Hero> </Hero>{" "}
+      <Card
+        img="katie-zaferes.png"
+        rating={5.0}
+        reviewCount={20}
+        price={201}
+        country="USA"
+        title="Life Lessons with Katie Zaferes"
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import Leader from '../images/image12.png'
+import React from "react";
+// import Leader from "../images/image12.png";
 export default function Card(props) {
-	return (
-		<div className='card'>
-			<img src={Leader} alt="" />
-			<p className='card-stat'><i className='bi bi-star-fill'></i>
-				<span>5.0</span>
-				<span>60&nbsp;·</span>
-				<span>USA</span>
-			</p>
-			<p className='card-des'>Life lessons with Katie Zaferes.</p>
-			<p className='card-footer'><strong>From $136</strong> / person</p>
-		</div>
-	)
+  return (
+    <div className="card">
+      <img src={require(`../images/${props.img}`)} className="card--image" />
+      <p className="card-stat">
+        <i className="bi bi-star-fill"></i>
+        <span>{props.rating}</span>
+        <span>{props.reviewCount}&nbsp;·</span>
+        <span>{props.country}</span>
+      </p>
+      <p className="card-des">{props.title}</p>
+      <p className="card-footer">
+        <strong>From ${props.price}</strong> / person
+      </p>
+    </div>
+  );
 }
